667a3ed3166ec3db71a7ad36780c2d098d529fa8
Adrien Loup <adloup@gmail.com>
2025-07-26 12:17:09 +0200

    feat(1): initialization ppc app

--- src/domains/settings/infrastructure/setti.context.ts
+++
@@ -1,5 +0,0 @@
-import { type Dispatch, createContext } from 'react';
-import type { SettingsDispatch, Settings } from '@/src/domains/settings/domain/setti.type.ts';
-
-export const SettiContext = createContext<Settings | undefined>(undefined);
-export const SettiDisContext = createContext<Dispatch<SettingsDispatch> | undefined>(undefined);
--- src/domains/settings/infrastructure/setti.state.ts
+++
@@ -1,6 +0,0 @@
-import type { Settings } from '@/src/domains/settings/domain/setti.type.ts';
-
-export const SETTINGS_STATE: Settings = {
-  mode: 'system',
-  pause: false,
-};
