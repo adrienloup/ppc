114efc4c501ea833d4414c1cb40637fd0547ee4b
Adrien Loup <adloup@gmail.com>
2025-08-14 04:14:12 +0200

    feat(1): initialization ppc app

--- src/domains/resources/infrastructure/resources.provider.tsx
+++ src/domains/resources/infrastructure/resources.provider.tsx
@@ -1,17 +1,17 @@
 import { type FC, useCallback, useEffect, useReducer, useRef } from 'react';
 import { useAuth } from '@/src/domains/auth/interfaces/useAuth.ts';
 import { useProfile } from '@/src/domains/profile/interfaces/useProfile.ts';
-import { resourcesReducer } from '@/src/domains/resources/application/resources.reducer.ts';
-import { ResourcesContext, ResourcesDisContext } from '@/src/domains/resources/infrastructure/resources.context.tsx';
-import { RESOURCES_KEY } from '@/src/domains/resources/infrastructure/resources.key.ts';
-import { RESOURCES_STATE } from '@/src/domains/resources/infrastructure/resources.state.ts';
+import { wireReducer } from '@/src/domains/wire/application/wire.reducer.ts';
+import { ResourcesContext, ResourcesDisContext } from '@/src/domains/wire/infrastructure/resources.context.tsx';
+import { RESOURCES_KEY } from '@/src/domains/wire/infrastructure/resources.key.ts';
+import { RESOURCES_STATE } from '@/src/domains/wire/infrastructure/resources.state.ts';
 import { useInterval } from '@/src/shared/hooks/useInterval.ts';
 import { useLocalStorage } from '@/src/shared/hooks/useLocalStorage.ts';
 import type { Children } from '@/src/shared/types/children.type.ts';
 
 export const ResourcesProvider: FC<{ children: Children }> = ({ children }) => {
   const resourceStorage = useLocalStorage(RESOURCES_KEY, RESOURCES_STATE);
-  const [state, dispatch] = useReducer(resourcesReducer, resourceStorage.get());
+  const [state, dispatch] = useReducer(wireReducer, resourceStorage.get());
   const { user, users } = useAuth();
   const userRef = useRef<string | null>(user);
   const { pause } = useProfile();
